f42afaa28d6cbaac70f8cd73051b0a758797c1a7
MrGuss <bednyakov04@mail.ru>
2023-01-30 21:13:55 +0300

    Upd

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,6 +7,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Запуск Polybar со стандартным расположением конфигурационного файла в ~/.config/polybar/config
-polybar mybar &
-
-echo "Polybar загрузился..."
+#polybar mybar &
+sleep 1
+#echo "Polybar загрузился..."
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload mybar &
+  done
+else
+  polybar --reload mybar
+fi
